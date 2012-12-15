ed8425b31ede773ac4f4182c756c1d573be7137a
takepu <takepuxp@gmail.com>
2012-12-15 18:30:53 +0900

    VB版完成

---
+++ ToDo_for_VB/ToDo_for_VB/StringList.vb
@@ -0,0 +1,49 @@
+﻿Public Class StringList
+    '//-----------------------------------------------------------------------
+    '// コンストラクタ
+    '//-----------------------------------------------------------------------
+    Public Sub New()
+
+    End Sub
+
+    '//-----------------------------------------------------------------------
+    '// 終了
+    '//-----------------------------------------------------------------------
+    Protected Overrides Sub Finalize()
+    End Sub
+
+    '//-----------------------------------------------------------------------
+    '// 文字列追加
+    '//-----------------------------------------------------------------------
+    Public Sub Add(ByVal str As String)
+        Dim max As Integer
+        If IsNothing(StrListObj) Then
+            max = 0
+        Else
+            max = StrListObj.Length
+        End If
+
+        ReDim Preserve StrListObj(max)
+        StrListObj(max) = str
+    End Sub
+
+    '//-----------------------------------------------------------------------
+    '// 文字列取得
+    '//-----------------------------------------------------------------------
+    Public Function GetStr(ByVal idx As Integer) As String
+        If idx >= StrListObj.Length Then
+            GetStr = ""
+        Else
+            GetStr = StrListObj(idx)
+        End If
+    End Function
+
+    '//-----------------------------------------------------------------------
+    '// リスト長取得
+    '//-----------------------------------------------------------------------
+    Public Function GetLength() As Integer
+        GetLength = StrListObj.Length
+    End Function
+
+    Private StrListObj() As String
+End Class
---
+++ ToDo_for_VB/ToDo_for_VB/ToDo.vb
@@ -0,0 +1,11 @@
+﻿Module ToDo
+
+    Sub Main()
+        Dim Appl As Application = New Application
+
+        Appl.Initial()
+        Appl.Run()
+        Appl.ExitFunc()
+    End Sub
+
+End Module
